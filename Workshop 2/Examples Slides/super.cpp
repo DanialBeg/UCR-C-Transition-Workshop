eb0a74ec5511a8cf59cd026ae02cb9cee5bbce89
drena001 <43687363+DRenales@users.noreply.github.com>
2019-10-18 13:42:22 -0700

    Create super.cpp

---
+++ Workshop 2/Examples Slides/super.cpp
@@ -0,0 +1,27 @@
+#include <string>
+
+class Person {
+  private:
+    std::string name;
+  public:
+    Person() {
+      this->name = "Person";
+    }
+    std::string getName() { return this->name; };
+};
+
+class Student : public Person {
+  private:
+    std::string name;
+  public:
+    Student() {
+      this->name = "Student";
+    }
+    std::string getName() { return this->name; };
+};
+
+int main() {
+  student* s = new Student();
+  std::cout << s->Persons::getName() << std::endl;  // Prints "Person"
+  std::cout << s->getName() << std::endl;           // Prints "Student"
+}
